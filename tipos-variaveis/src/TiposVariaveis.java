6d182db06ca6fc2f5ac9d780bf0a4a08663fc7c5
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2022-10-01 20:32:35 -0300

    alguns exemplos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
+        //tipos primitivos
+        //estudar a classe String que representa texto na apçlicação
+
+        String meuNome = "AMANDA LINO";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto  = 1;
+        int numeroNormal = numeroCurto;
+        final short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14;
+        
     }
 }
