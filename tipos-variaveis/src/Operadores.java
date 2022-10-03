9fa207071e2c79834cc10eb5166230afffdce469
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2022-10-03 19:41:26 -0300

    atualizações

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,86 @@
+import javax.lang.model.util.ElementScanner6;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        //classe de operadores
+
+        //double soma = 10.5 + 15.7;
+        //int subtracao = 113 - 25;
+        //int multiplicacao = 20 * 7;
+        //int divisao = 15/3;
+        //int modulo = 18 % 3;
+        //double resultado = (10 *7) + (20/4);
+
+        //tipo texto, precisa fazer a concatenação
+
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        //concatenação
+
+        String concatenacao = "?";
+
+        //concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        //concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        //concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        //concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        //concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+
+        //Operadores Unários
+            //(+)de valor positivo
+            //(-)de valor negativo
+            //(++)de incremento de valor
+            //(--)de decremento de valor
+            //(!)de lógico de negação
+
+        //int numero = 5;
+
+        //numero = - numero;
+        //System.out.println(numero);
+
+       // numero = numero * -1;
+        //System.out.println(numero);
+
+
+        //incremento de números
+        //fluxo de repetição
+
+        //numero = numero + 1; OU TAMBÉM:
+        //numero++;
+        //System.out.println(numero);
+
+        //booleanos
+        //boolean variavel = true;
+        //variavel = !variavel;
+
+        //System.out.println(variavel);
+
+        //Operador Ternário
+
+        int a,b;
+        a = 6;
+        b = 6;
+        String resultado = a == b ? "verdadeiro" : "falso";
+        
+
+        System.out.println(resultado);
+
+        //Operadores Relacionais
+
+        
+
+    }
+}
